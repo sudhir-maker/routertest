a1f9c41a9dcbb71c16ac30592dd706df7d2a16a2
sudhir-maker <sudhirsah98242@gmail.com>
2026-06-02 12:12:30 +0900

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -8,7 +8,7 @@ return(
         list.map((product) => (
             <div key={product.id} className="product-item">
                 <h3>{product.name}</h3>
-                <img src={product.image} alt={product.name} width={150} />
+                <img src="/routertest/images/laptop.jpeg"alt={product.name} width={150} />
                 <p>{product.price}</p>
                 <Link to={`/productlist/${product.id}`}>View Details</Link>
             </div>
